3c08c8a3cd4e290b538c7d01ccf92336278f5dd2
David Lynam <dlynam@gmail.com>
2013-11-24 23:47:48 -0800

    refactored

--- lib/repeat.rb
+++ lib/repeat.rb
@@ -7,19 +7,18 @@ class Repeat
 
   #finds the first non-repeating character in a string
   def first_non_repeating
-    @index = 0
-    @repeat_count = 1
-    @input.size.times do
-      if @input[@index] == @input[@index+1]
-        @repeat_count += 1
+    repeat_count = 1
+    input.split("").each_index{ |index|
+      if input[index] == input[index+1]
+        repeat_count += 1
       else
-        if @repeat_count == 1
-          puts "First non-repeating character is #{@input[@index]}"
+        if repeat_count == 1
+          puts "First non-repeating character is #{input[index]}"
+          break
         else
-          @repeat_count = 1
+          repeat_count = 1
         end
       end
-      @index += 1
-    end
+    }
   end
 end
